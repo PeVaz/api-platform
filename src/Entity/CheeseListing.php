f6ace5a65c8b0ef9ca557f62024c34cb04b3e327
PeVaz <pedro.vazquez@teltek.es>
2020-08-04 11:32:51 +0200

    Added new functions to Entity

--- src/Entity/CheeseListing.php
+++ src/Entity/CheeseListing.php
@@ -5,12 +5,13 @@ namespace App\Entity;
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\CheeseListingRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Carbon\Carbon;
 
 /**
  *@ApiResource(
  *     collectionOperations={"get", "post"},
  *     itemOperations={
- *      "get"={"path"="/i❤️cheeses/{id}"},
+ *      "get"={},
  *      "put"
  * },
  *     shortName="cheeses"
@@ -52,6 +53,11 @@ class CheeseListing
      */
     private $isPublished;
 
+    public function __construct()
+    {
+        $this->createdAt = new \DateTimeImmutable();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -74,10 +80,9 @@ class CheeseListing
         return $this->description;
     }
 
-    public function setDescription(string $description): self
+    public function setTextDescription(string $description): self
     {
-        $this->description = $description;
-
+        $this->description = nl2br($description);
         return $this;
     }
 
@@ -98,11 +103,9 @@ class CheeseListing
         return $this->createdAt;
     }
 
-    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    public function getCreatedAtAgo(): string
     {
-        $this->createdAt = $createdAt;
-
-        return $this;
+        return Carbon::instance($this->getCreatedAt())->diffForHumans();
     }
 
     public function getIsPublished(): ?bool
